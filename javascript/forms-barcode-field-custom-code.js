7f8c07afbc56994d8248405a35e74da8f63e12f1
jt <jt@blinkmobile.com.au>
2012-10-31 17:35:40 +1100

    Changed line comments to block comments

--- javascript/forms-barcode-field-custom-code.js
+++ javascript/forms-barcode-field-custom-code.js
@@ -1,31 +1,33 @@
-//
-// Step 3 custom code for reading Barcodes into a form field (native apps only).
-// To be added as manual calculation code for Calculation fields.
-//
+/*
+ * Step 3 custom code for reading Barcodes into a form field (native apps only).
+ * To be added as manual calculation code for Calculation fields.
+ *
+ * NB: Javascript line comments should *NOT* be used in custom code added to forms.
+ */
 
 function() {
     var barcodereader = window.cordova.require(
             'cordova/plugin/BGBarcodeReader'),
         bfo = this;
 
-    //
-    // Function to be called after successfully reading a barcode.
-    //
+    /*
+     * Function to be called after successfully reading a barcode.
+     */
     var onSuccess = function(message) {
         bfo.setFieldValue("BarcodeValue", message);
-        // Please replace 'BarcodeValue' with the name of the target field
-        // in the form.
+        /* Please replace 'BarcodeValue' with the name of the target field
+           in the form. */
 
         bfo.setFieldValue("readBarcode", "");
-        // Please replace 'readBarCode' with the name of the Calculation
-        // field where the custom code will be inserted.
+        /* Please replace 'readBarCode' with the name of the Calculation
+           field where the custom code will be inserted. */
 
         console.log(message);
     };
-        
-    //
-    // Function to be called after an error.
-    //
+
+    /*
+     * Function to be called after an error.
+     */
     var onError = function(message) {
         console.log(message);
         navigator.notification.alert(message);
