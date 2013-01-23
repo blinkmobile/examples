63471ce27124c5b263fb8ba2a96fc05e2886398a
Ray Pearce <ray@rayron.com.au>
2013-01-23 12:18:50 +1100

    Changes To FindfLocation

--- madl interaction/FindLocation.php
+++ madl interaction/FindLocation.php
@@ -1,34 +1,28 @@
-$answerSpace = 'support'; // the answerSpace you are in. 
-
-The answerspace cannot have spaces or underscores in the names.
-
-$interaction = 'gps'; // the interaction you are editing  no spaces or underscores allowed!
+$answerSpace = 'support'; // the answerSpace you are in
+$interaction = 'gps'; // the interaction you are editing NOTE: no spaces allowed!
 
 if($_POST['coords']){
 
-// you get in here after a successful GeoLocation read 
+	// you get in here after a successful GeoLocation read 
 
-//$t->setSessionValue('rawLocation', $_POST); // save raw location
+	
+	//$t->setSessionValue('rawLocation', $_POST); // save raw location
 
-// use Google Reverse Geocoding
+	// use Google Reverse Geocoding
+	$latitude = $_POST['coords']['latitude'];
+	$longitude = $_POST['coords']['longitude'];
+	$t->fetch('http://maps.googleapis.com/maps/api/geocode/json?latlng=' . $latitude . ',' . $longitude . '&sensor=false');
+	
+	
+//	$t->setSessionValue('locationParts', $t->result); // save location parts
 
-$latitude = $_POST['coords']['latitude'];
+	$address_parts = json_decode($t->result);
 
-$longitude = $_POST['coords']['longitude'];
+	$street_address = $address_parts->results[0]->formatted_address;
 
-$t->fetch('http://maps.googleapis.com/maps/api/geocode/json?latlng=' . $latitude . ',' . $longitude . '&sensor=false');
-
-// $t->setSessionValue('locationParts', $t->result); // save location parts
-
-$address_parts = json_decode($t->result);
-
-$street_address = $address_parts->results[0]->formatted_address;
-
-return 'Your location is ' . $street_address . ' with Latitude = '.$latitude .' and Longitude = '.$longitude;
-
-
-
-$theorigin = $longitude. " ' ". $longitude;
+	return 'Your location is ' . $street_address . '  with Latitude =  '.$latitude .' and Longitude =  '.$longitude;
+	
+	$theorigin = $longitude. " ' ". $longitude;
 
 }
 
@@ -39,26 +33,15 @@ $val = $t->getSessionValue('gps');
 $temp = <<<ABC
 
 <script type="text/javascript">
-
 (function(window, undefined) {
-
 var $ = window.jQuery,
-
-navigator = window.navigator,
-
-geolocation,
-
-options = {
-
-enableHighAccuracy: true,
-
-timeout: 10 * 1000, // take no longer than 10 seconds
-
-maximumAge: 30 * 1000 // accept locations that are 30 seconds old
-
-};
-
-
+  navigator = window.navigator,
+  geolocation,
+  options = {
+    enableHighAccuracy: true,
+    timeout: 10 * 1000, // take no longer than 10 seconds
+    maximumAge: 30 * 1000 // accept locations that are 30 seconds old
+  };
 
 function onSuccess(position) {
  var location;
@@ -84,94 +67,77 @@ function onSuccess(position) {
 	}
 }
 
-
-
 function onError(error) {
-
-// do something, Google or see documentation for examples
-
-$("#geolocationoutput").html('GeoLocation failed.');
-
+  // do something, Google or see documentation for examples
+  $("#geolocationoutput").html('GeoLocation failed.');
 }
 
 if (typeof $ !== 'undefined' && $.type(navigator) === 'object') {
-
-geolocation = navigator.geolocation;
-
-if ($.type(geolocation) === 'object') {
-
-// waiting before doing location stuff, just in case
-
-setTimeout(function() {
-
-geolocation.getCurrentPosition(onSuccess, onError, options);
-
-}, 1000);
-
+  geolocation = navigator.geolocation;
+  if ($.type(geolocation) === 'object') {
+    // waiting before doing location stuff, just in case
+    setTimeout(function() {
+      geolocation.getCurrentPosition(onSuccess, onError, options);
+    }, 1000);
+  }
+  else {
+    alert('This content requires features that are not available on your device or browser.');
+  }
 }
-
 else {
-
-alert('This content requires features that are not available on your device or browser.');
-
-}
-
-}
-
-else {
-
-alert('This content requires features that are not available on your device or browser.');
-
+  alert('This content requires features that are not available on your device or browser.');
 }
 
 }(this));
-
 </script>
-
 <div id='geolocationoutput'>Acquiring GeoLocation...</div>
-
 <noscript><p>This content requires JavaScript to be enabled.</p></noscript>
-
 ABC;
 
 return $temp;
 
+
+
+
 if($args[0]=='1') {
 
 $mapOptions = Array(
-
-'origin' => $theorigin,
-
-'destination' => '1 Eastern Avenue, Bilinga QLD 4225',
-
-'sensor' => ($args[0]=='1'?true:false)
-
+   'origin' => $theorigin,
+   'destination' => '1 Eastern Avenue, Bilinga QLD 4225',
+   'sensor' => ($args[0]=='1'?true:false)
 );
 
 $t->map('directions', $mapOptions);
-
 $t->prepend('<h2>Directions to Coolangatta Airport</h2><h4>From: ' . ($args[0]=='1'?'Your current location':$args[0]) . '</h4><br>');
-
 return $t->result ;
 
+
+
 } else {
+
 
 // use location from selection
 
 $mapOptions = Array(
-
-'origin' => $args[0],
-
-'destination' => '1 Eastern Avenue, Bilinga QLD 4225',
-
-'sensor' => ($args[0]!='1'?true:false)
-
+   'origin' => $args[0],
+   'destination' => '1 Eastern Avenue, Bilinga QLD 4225',
+   'sensor' => ($args[0]!='1'?true:false)
 );
-
 $t->map('directions', $mapOptions);
-
 $t->prepend('<h2>Directions to Coolangatta Airport</h2><h4>From: ' . ($args[0]=='1'?'Your current location':$args[0]) . '</h4><br>');
-
 return $t->result ;
 
-}; 
+
+
+
+
+};
+
+
+
+
+
+
+
+
+
