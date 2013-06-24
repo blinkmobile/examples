043850e873856a5dc9f1325f7c0d662bad73af86
Ray Pearce <ray@rayron.com.au>
2013-06-24 16:46:25 +1000

    twitter example updated
    twitter example updated for new oAuth API

--- madl interaction/Social Media/TwitterStatusInteraction.php
+++ madl interaction/Social Media/TwitterStatusInteraction.php
@@ -1,12 +1,62 @@
-$screen_name = "blinkmob";
+/* The new twitter api requires an oAuth connection for all twitter functions.
+You must register for an app at https://dev.twitter.com and get oAuth credentials.
+*/
 
-// And the number of tweets you want to be returned.
+// code for your mADL interaction.
 
-$count = "10";
 
-// The rest is already done.
+function buildBaseString($baseURI, $method, $params) {
+    $r = array();
+    ksort($params);
+    foreach($params as $key=>$value){
+        $r[] = "$key=" . rawurlencode($value);
+    }
+    return $method."&" . rawurlencode($baseURI) . '&' . rawurlencode(implode('&', $r));
+}
 
-$t->fetch("https://api.twitter.com/1/statuses/user_timeline.json?include_entities=true&include_rts=true&screen_name=".$screen_name."&count=".$count);
+function buildAuthorizationHeader($oauth) {
+    $r = 'Authorization: OAuth ';
+    $values = array();
+    foreach($oauth as $key=>$value)
+        $values[] = "$key=\"" . rawurlencode($value) . "\"";
+    $r .= implode(', ', $values);
+    return $r;
+}
+
+$url = "https://api.twitter.com/1.1/statuses/user_timeline.json";
+
+/*
+provide the required credentials from  https://dev.twitter.com
+*/
+
+$oauth_access_token = "YOUR ACCESS TOKEN";
+$oauth_access_token_secret = "YOUR ACCESS TOKEN SECRET";
+$consumer_key = "YOUR CONSUMER KEY";
+$consumer_secret = "YOUR CONSUMER SECRET";
+
+$oauth = array( 'oauth_consumer_key' => $consumer_key,
+                'oauth_nonce' => time(),
+                'oauth_signature_method' => 'HMAC-SHA1',
+                'oauth_token' => $oauth_access_token,
+                'oauth_timestamp' => time(),
+                'oauth_version' => '1.0');
+
+$base_info = buildBaseString($url, 'GET', $oauth);
+$composite_key = rawurlencode($consumer_secret) . '&' . rawurlencode($oauth_access_token_secret);
+$oauth_signature = base64_encode(hash_hmac('sha1', $base_info, $composite_key, true));
+$oauth['oauth_signature'] = $oauth_signature;
+
+// Make Requests
+$header = array(buildAuthorizationHeader($oauth), 'Expect:');
+$options = array( CURLOPT_HTTPHEADER => $header,
+                  //CURLOPT_POSTFIELDS => $postfields,
+                  CURLOPT_HEADER => false,
+                  CURLOPT_URL => $url,
+                  CURLOPT_RETURNTRANSFER => true,
+                  CURLOPT_SSL_VERIFYPEER => false);
+                  
+                  
+$t->fetch($url,"", $header,"","" ,"",$options);
 
 $json = json_decode($t->result);
 
@@ -33,6 +83,14 @@ echo"</td></tr> ";
 echo"</table>";
 
 echo '</div>';
+
+
+
+
+
+
+
+
 
 /*
 You also need to place some CSS in the interactions Advanced->Style sheet area.
