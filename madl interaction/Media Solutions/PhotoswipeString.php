15dce56fa3ae2406495e149abc415cf9600d54f8
Ray Pearce <ray@rayron.com.au>
2013-08-13 15:51:59 +1000

    Photo Gallery

---
+++ madl interaction/Media Solutions/PhotoswipeString.php
@@ -0,0 +1,105 @@
+$html = '<div id="Gallery">
+      <table border="0" cellspacing="2" cellpadding="0">
+        <tr>
+        <div class="gallery-row">
+          <td><div class="gallery-item"><a href="http://www.wavesresorts.com.au/img.ashx?f=v&v=554223"><img src="http://www.wavesresorts.com.au/img.ashx?f=v&v=554223" alt="Image 001" height="100" width="100" /></a></div>
+      </td>
+          <td><div class="gallery-item"><a href="http://www.wavesresorts.com.au/img.ashx?f=v&v=557227"><img src="http://www.wavesresorts.com.au/img.ashx?f=v&v=557227" alt="Image 005" height="100" width="100" /></a></div>
+      </td>
+          <td><div class="gallery-item"><a href="http://www.wavesresorts.com.au/img.ashx?f=v&v=557230"><img src="http://www.wavesresorts.com.au/img.ashx?f=v&v=557230" alt="Image 003" height="100" width="100" /></a></div>
+    </td>
+      </div>
+  
+        </tr>
+        <tr>
+        <div class="gallery-row">
+          <td>
+            <div class="gallery-item">
+              <a href="http://www.wavesresorts.com.au/img.ashx?f=v&v=555189"><img src="http://www.wavesresorts.com.au/img.ashx?f=v&v=555189" alt="Image 004" height="100" width="100" /></a></div>
+          </td>
+          <td>
+            <div class="gallery-item">
+              <a href="http://www.wavesresorts.com.au/img.ashx?f=v&v=555194"><img src="http://www.wavesresorts.com.au/img.ashx?f=v&v=555194" alt="Image 005" height="100" width="100" /></a></div>
+          </td>
+          <td>
+            <div class="gallery-item">
+              <a href="http://www.wavesresorts.com.au/img.ashx?f=v&v=555195"><img src="http://www.wavesresorts.com.au/img.ashx?f=v&v=555195" alt="Image 006" height="100" width="100" /></a></div>
+          </td>
+        </div>
+  
+        </tr>
+        <tr>
+          <div class="gallery-row">
+          <td>
+            <div class="gallery-item">
+              <a href="http://www.wavesresorts.com.au/img.ashx?f=v&v=555196"><img src="http://www.wavesresorts.com.au/img.ashx?f=v&v=555196" alt="Image 007" height="100" width="100" /></a></div>
+          </td>
+          <td>
+            <div class="gallery-item">
+              <a href="http://www.wavesresorts.com.au/img.ashx?f=v&v=555197"><img src="http://www.wavesresorts.com.au/img.ashx?f=v&v=555197" alt="Image 008" height="100" width="100" /></a></div>
+          </td>
+          <td>
+            <div class="gallery-item">
+              <a href="http://www.wavesresorts.com.au/img.ashx?f=v&v=555220"><img src="http://www.wavesresorts.com.au/img.ashx?f=v&v=555220" alt="Image 009" height="100" width="100" /></a></div>
+          </td>
+        </div>
+        </tr>
+        <tr>
+          <div class="gallery-row">
+          <td>
+            <div class="gallery-item">
+              <a href="http://www.wavesresorts.com.au/img.ashx?f=v&v=555225"><img src="http://www.wavesresorts.com.au/img.ashx?f=v&v=555225" alt="Image 010" height="100" width="100" /></a></div>
+          </td>
+          <td>
+            <div class="gallery-item">
+              <a href="http://www.wavesresorts.com.au/img.ashx?f=v&v=555226"><img src="http://www.wavesresorts.com.au/img.ashx?f=v&v=555226" alt="Image 011" height="100" width="100" /></a></div>
+          </td>
+          <td>
+            <div class="gallery-item">
+              <a href="http://www.wavesresorts.com.au/img.ashx?f=v&v=555227"><img src="http://www.wavesresorts.com.au/img.ashx?f=v&v=555227" alt="Image 012" height="100" width="100" /></a></div>
+          </td>
+        </div>
+        </tr>
+        <tr>
+          <div class="gallery-row">
+          <td>
+            <div class="gallery-item">
+              <a href="http://www.wavesresorts.com.au/img.ashx?f=v&v=555228"><img src="http://www.wavesresorts.com.au/img.ashx?f=v&v=555228" alt="Image 013" height="100" width="100" /></a></div>
+          </td>
+          <td>
+            <div class="gallery-item">
+              <a href="http://www.wavesresorts.com.au/img.ashx?f=v&v=555206"><img src="http://www.wavesresorts.com.au/img.ashx?f=v&v=555206" alt="Image 014" height="100" width="100" /></a></div>
+          </td>
+          <td>
+            <div class="gallery-item">
+              <a href="http://www.wavesresorts.com.au/img.ashx?f=v&v=555207"><img src="http://www.wavesresorts.com.au/img.ashx?f=v&v=555207" alt="Image 015" height="100" width="100" /></a></div>
+          </td>
+        </div>
+        </tr>
+        <tr>
+          <div class="gallery-row">
+          <td>
+            <div class="gallery-item">
+              <a href="http://www.wavesresorts.com.au/img.ashx?f=v&v=555208"><img src="http://www.wavesresorts.com.au/img.ashx?f=v&v=555208" alt="Image 016" height="100" width="100" /></a></div>
+          </td>
+          <td>
+            <div class="gallery-item">
+              <a href="http://www.wavesresorts.com.au/img.ashx?f=v&v=555267"><img src="http://www.wavesresorts.com.au/img.ashx?f=v&v=555267" alt="Image 017" height="100" width="100" /></a></div>
+          </td>
+          <td>
+            <div class="gallery-item">
+              <a href="http://www.wavesresorts.com.au/img.ashx?f=v&v=555270"><img src="http://www.wavesresorts.com.au/img.ashx?f=v&v=555270" alt="Image 018" height="100" width="100" /></a></div>
+          </td>
+        </div>
+        </tr>
+        
+      </table>
+      </div>';
+
+$html .= "<script type=\"text/javascript\">
+setTimeout(function() {
+Code.photoSwipe('a', '#Gallery');
+}, 503);
+</script>";
+
+return $html;
