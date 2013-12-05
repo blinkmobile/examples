172d5cc078076acc3e3cf1e24f5ccad5afc97634
Ray Pearce <ray@rayron.com.au>
2013-12-05 16:17:04 +1100

    New Image Annotations Support For Native Apps

---
+++ javascript/setAnnotationsSupport.js
@@ -0,0 +1,32 @@
+/**
+ * SET CAMERA / IMAGE ANNOTATIONS FOR BLINK FORMS
+ * Modified Ray Pearce 5/12/2013  
+ * iOS devices Only at this time
+ * Will work with Blink Native Client is added in 2.2.1 or  (Master App 1.12)
+ * 
+ * Answerspace setup=>Native Application=>camera options must be set to allowEdit	true
+ * 
+ * This script will allow you to add annotations support for Blink forms
+ * Add script below to HTML HEAD in answerspace setup to allow all camera / image fields
+ * to allow annotations. 
+ */
+
+
+console.log('Checking to support annotations');
+ if (_Blink.isBlinkGap === true) {
+    	if (currentConfig.cameraOptions) {
+		 	 	currentConfig.cameraOptions = JSON.stringify(_.extend(JSON.parse(currentConfig.cameraOptions), {"shouldAnnotate":true}));
+     	 	console.log('cameraOptions extended to support annotations');
+    	} else {
+     	 	currentConfig.cameraOptions = {"shouldAnnotate":true}
+      	console.log('cameraOptions created to support annotations');
+    	}
+  	}
+  
+
+
+
+ * with the 2013M31 RC release, it should be possible to use these by programatically changing the camera plugin options on form load or on form field button press. The following code will add the shouldAnnotate option to the answerSpace configuration: * to allow annotations. 
+ */
+ 
+ currentConfig.cameraOptions = JSON.stringify(_.extend(JSON.parse(currentConfig.cameraOptions), {"shouldAnnotate":true}))
