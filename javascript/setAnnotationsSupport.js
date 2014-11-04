780aaa48cd8943ada066f55d69959796a0260905
Ray Pearce <ray@rayron.com.au>
2014-11-04 12:28:38 +1100

    Android Support For Image Annotations

--- javascript/setAnnotationsSupport.js
+++ javascript/setAnnotationsSupport.js
@@ -1,44 +1,46 @@
-/**
- * SET CAMERA / IMAGE ANNOTATIONS FOR BLINK FORMS
- * Modified Ray Pearce 3/2/2014 
- * iOS devices Only at this time
- * Will work with Blink Native Client is added in 2.2.1 or  (Master App 1.12)
- * 
+  /**
+ * SET CAMERA / IMAGE ANNOTATIONS FOR BMP
+ * Modified Ray Pearce 3/11/2014 
+ * BIC2 Only at this stage.
  * Answerspace setup=>Native Application=>camera options must be set to allowEdit	true
  * 
  * This script will allow you to add annotations support for Blink forms
  * Add script below to HTML HEAD in answerspace setup to allow all camera / image fields
  * to allow annotations. 
  */
+ 
+ 
+<script>
 
+  $(document).on('viewReady', function() {
+
+  try{
 
 var old_updateCurrentConfig = updateCurrentConfig;
-
 updateCurrentConfig = function() {
+
 //call the existing updateCurrentConfig
 old_updateCurrentConfig();
-
 console.log('enabling image annotations'); 
+
 //enable image annotation
 if (currentConfig.cameraOptions) {
 currentConfig.cameraOptions = JSON.stringify(_.extend(JSON.parse(currentConfig.cameraOptions), {"shouldAnnotate":true}));
 console.log('cameraOptions extended to support annotations');
 } else {
-currentConfig.cameraOptions = '{"shouldAnnotate":true}';
+	currentConfig.cameraOptions = '{"shouldAnnotate":true}';
 console.log('cameraOptions created to support annotations');
 }
 }
 
+
+ 
+  currentConfig.cameraOptions = JSON.stringify(_.extend(JSON.parse(currentConfig.cameraOptions), {"shouldAnnotate":true}))
+ 
+  }catch (e) {// END try  
+    log(e);
+  }
   
-
-
-
- /* 
-  * with the 2013M31 RC release, it should be possible to use these by programatically changing
-  * the camera plugin options on form load or on form field button press. The following code will
-  * add the shouldAnnotate option to the answerSpace configuration: * to allow annotations. 
- */
- 
- currentConfig.cameraOptions = JSON.stringify(_.extend(JSON.parse(currentConfig.cameraOptions), {"shouldAnnotate":true}))
- 
- 
+});
+  
+</script>
